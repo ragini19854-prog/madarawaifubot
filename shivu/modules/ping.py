d2f3e050857905a5737f9489fb244a28abdfa5cd
ragini19854-prog <ragini.19854@gmail.com>
2026-04-23 12:01:34 +0530

    Update ping.py

--- shivu/modules/ping.py
+++ shivu/modules/ping.py
@@ -1,18 +1,43 @@
 import time
-
+import datetime
 from telegram import Update
-from telegram.ext import CommandHandler, CallbackContext
+from telegram.ext import CommandHandler, ContextTypes
 
-from shivu import application, sudo_users
+from shivu import application
 
-async def ping(update: Update, context: CallbackContext) -> None:
-    if str(update.effective_user.id) not in sudo_users:
-        update.message.reply_text("Nouu.. its Sudo user's Command..")
-        return
-    start_time = time.time()
-    message = await update.message.reply_text('Pong!')
-    end_time = time.time()
-    elapsed_time = round((end_time - start_time) * 1000, 3)
-    await message.edit_text(f'Pong! {elapsed_time}ms')
+START_TIME = time.time()
+
+
+async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    start = time.time()
+
+    msg = await update.message.reply_text("🏓 Pinging...")
+
+    end = time.time()
+    ping_ms = round((end - start) * 1000, 2)
+
+    # uptime
+    uptime_sec = int(time.time() - START_TIME)
+    uptime = str(datetime.timedelta(seconds=uptime_sec))
+
+    # dynamic values
+    user_name = update.effective_user.first_name
+    bot_name = context.bot.first_name
+    chat_name = update.effective_chat.title or "Private Chat"
+
+    await msg.edit_text(
+        f"""
+╭━〔 {bot_name} 〕━⬣
+┃ 🏓 **PING STATUS**
+┃ ⚡ Speed: `{ping_ms} ms`
+┃ 🕒 Uptime: `{uptime}`
+┃ 👤 User: **{user_name}**
+╰━━━━━━━━━━━━━━━⬣
+
+❝ ❤️ Powered By: **madara** ❞  
+❝ 💬 Chat: **{chat_name}** ❞
+"""
+    )
+
 
 application.add_handler(CommandHandler("ping", ping))
